e34e33b4535a6b4a560dff3820fbae89824e756a
Mattias <mattiash91@gmail.com>
2022-11-16 14:08:12 +0100

    Mer kommentarer

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 //Importerar Scanner
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Skriv in kostnaden: ");
+        int summa = input.nextInt();
+        //Sparar summan
+
+        System.out.print("Skriv in betalsumman: ");
+        int kontantKvar = input.nextInt() - summa;
+        //Sparar summan som ska betalas
+
+        System.out.println("Totalt tillbaka: " + kontantKvar + ":-");
+        System.out.println("Valörer: " + sedelMetod("", kontantKvar));
+    }
     static String sedelMetod(String retur, int kontantKvar){
         int[] delar = {1000, 500, 200, 100, 50, 20, 2, 1};
         //Alla valörer sparade i en array som heter delar[i]
@@ -32,19 +45,5 @@ public class Main {
         }
         retur = retur.replaceFirst(",", " ");
         return retur.trim();
-    }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Skriv in kostnaden: ");
-        int summa = input.nextInt();
-        //Sparar summan
-
-        System.out.print("Skriv in betalsumman: ");
-        int kontantKvar = input.nextInt() - summa;
-        //Sparar summan som ska betalas
-
-        System.out.println("Totalt tillbaka: " + kontantKvar + ":-");
-        System.out.println("Valörer: " + sedelMetod("", kontantKvar));
     }
 }

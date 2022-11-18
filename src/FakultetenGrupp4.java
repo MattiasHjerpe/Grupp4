1e2c0d25f79d5af654436f18df5dc32eabb07fe1
EmeHag <emehag-2@student.ltu.se>
2022-11-18 12:06:21 +0100

    RÄDDADE VÄRLDEN

--- src/FakultetenGrupp4.java
+++ src/FakultetenGrupp4.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
-
+// Importerar Scanner
 public class FakultetenGrupp4 {
     public static void main(String[] args) {
         System.out.print("Skriv in ett tal: ");
+        //Ber användaren skriva in ett tal
         Scanner scanner = new Scanner(System.in);
+        //Skapar ett object i Scanner
         int tal = scanner.nextInt(), fakultet = 1;
         //Initialiserar tal och fakultet
 
         if (tal < 0) {
             System.out.printf("Ogiltigt tal, vänligen skriv in ett positivt heltal%nSkriv in ett tal: ");
             tal = scanner.nextInt();
+            //Om talet är mindre än 0 skrivs ett felmeddelande ut och sedan ges möjligheten att skriva in ett giltligt tal
         }
         for (int i = 1; i <= tal; i++) {
+            //Loopar i till samma värde som tal
             fakultet = i * fakultet;
+            //Multiplicerar i med fakulteten
         }
         System.out.printf("%d ! = %d", tal, fakultet);
+        //Skriver ut tal! = fakulteten
     }
 }

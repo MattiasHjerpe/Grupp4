fed33b1c7011abb85f10036c0bd9e0b321d61483
Mattias <mattiash91@gmail.com>
2022-11-18 16:32:40 +0100

    Spelet

--- src/Examinationsuppgift2/Aventyrsspel.java
+++ src/Examinationsuppgift2/Aventyrsspel.java
@@ -1,21 +1,21 @@
 package Examinationsuppgift2;
 
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Aventyrsspel {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         meny();
     }
-    public static void meny() {
+    private static void meny() {
         Scanner input = new Scanner(System.in);
         System.out.printf("1. Starta nytt spel%n2. Avsluta%nVal: ");
-
-
             int menyval = input.nextInt();
             switch (menyval){
                 case 1:{
                     System.out.println("Startar spelet");
+                    setupGame();
                     break;
                 }
                 case 2:{
@@ -26,6 +26,9 @@ public class Aventyrsspel {
                     System.out.printf("Vänligen mata in 1 eller 2%n");
                 }
             }
-
     }
+    private static void setupGame() {
+        }
+
+
 }

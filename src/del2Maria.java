181b91a38232545f51a3df214e9a3fd5a01983c1
Mattias <mattiash91@gmail.com>
2022-11-18 11:13:36 +0100

    Ändrade string

--- src/del2Maria.java
+++ src/del2Maria.java
@@ -4,17 +4,17 @@ public class del2Maria {
     public static void main(String[] args) {
         System.out.print("Skriv in ett tal: ");
         Scanner scanner = new Scanner(System.in);
-        int tal = scanner.nextInt();
-        int fakultet = 1;
+        int tal = scanner.nextInt(), fakultet = 1;
+        //Initialiserar tal och fakultet
+
         if (tal < 0) {
-            System.out.println("Ogiltigt tal, vänligen skriv in ett positivt heltal");
-            System.out.print("Skriv in ett tal: ");
+            System.out.printf("Ogiltigt tal, vänligen skriv in ett positivt heltal%nSkriv in ett tal: ");
             tal = scanner.nextInt();
         }
         for (int i = 1; i <= tal; i++) {
             fakultet = i * fakultet;
         }
-        System.out.println(tal + "! = " + fakultet);
+        System.out.printf("%d ! = %d", tal, fakultet);
     }
 //test
 }

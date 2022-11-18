8eaeae7a86aba367f56068951a8314e5fc844386
MattiasHjerpe <mattiash91@gmail.com>
2022-11-18 10:39:50 +0100

    Update Del1.java

--- src/Del1.java
+++ src/Del1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 //Importerar Scanner
 
-public class Main {
+public class Del1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Skriv in kostnaden: ");

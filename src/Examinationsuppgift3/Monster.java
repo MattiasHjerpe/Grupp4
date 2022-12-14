93a59facd84bda9f3a658013b1b6a1e3c06757c5
Mattias <mattiash91@gmail.com>
2022-12-14 12:52:29 +0100

    Uppgift 3

--- src/Examinationsuppgift3/Monster.java
+++ src/Examinationsuppgift3/Monster.java
@@ -1,4 +1,64 @@
 package Examinationsuppgift3;
 
 public class Monster extends Fighter{
+    private String name, monsterType;
+    private int yPosition;
+    private int xPosition;
+    private int monsterHealth;
+    private int monsterStrength;
+    private int standardMonsterHealth = 20, standardMonsterStrength = 5, dragonHealth = 60, dragonStrength = 10;
+
+
+    public Monster(String name, int startYPosition, int startXPosition, String monsterType) {
+        setName(name);
+        setMonsterType(monsterType);
+        yPosition = startYPosition;
+        xPosition = startXPosition;
+    }
+
+    public int getMonsterStrength() {
+        return monsterStrength;
+    }
+
+    public int getMonsterHealth() {
+        return monsterHealth;
+    }
+
+    public void setMonsterStrength(int monsterStrength) {
+        this.monsterStrength = monsterStrength;
+    }
+
+    public void setMonsterHealth(int monsterStrength) {
+        this.monsterStrength = monsterStrength;
+    }
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMonsterType() {
+        return monsterType;
+    }
+
+    public void setMonsterType(String monsterType) {
+        this.monsterType = monsterType;
+        if (monsterType.equals("Dragon")){
+            setMonsterHealth(dragonHealth);
+            setMonsterStrength(dragonStrength);
+        } else {
+            setMonsterHealth(standardMonsterHealth);
+            setMonsterStrength(standardMonsterStrength);
+        }
+    }
 }

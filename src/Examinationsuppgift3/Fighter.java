0d72a10a829697725c4c15b44c1fe0f1f96b92c4
Mattias <mattiash91@gmail.com>
2022-12-15 11:15:38 +0100

    Uppgift 3

--- src/Examinationsuppgift3/Fighter.java
+++ src/Examinationsuppgift3/Fighter.java
@@ -4,7 +4,7 @@ public class Fighter {
     public void fightSequence(Player player, Monster monster){
         while (monster.getMonsterHealth() > 0){
             player.setPlayerHealth(monster.Attack(monster.getMonsterStrength(), player.getPlayerHealth()));
-            System.out.printf("%n%s attacks and deals %s damage!%n%s has %s healthpoints left.",
+            System.out.printf("%n%s attacks and deals \u001b[31m%s\u001b[0m damage!%n%s has %s healthpoints left.",
                     monster.getMonsterType(), monster.getMonsterStrength(), player.getName(), player.getPlayerHealth());
             try {
                 Thread.sleep(1000);
@@ -15,7 +15,7 @@ public class Fighter {
                 endGame();
             }
             monster.setMonsterHealth(player.Attack(player.getplayerStrength(), monster.getMonsterHealth()));
-            System.out.printf("%n%s attacks and deals %s damage!%n%s has %s healthpoints left.",
+            System.out.printf("%n%s attacks and deals \u001b[31m%s\u001b[0m damage!%n%s has %s healthpoints left.",
                     player.getName(), player.getplayerStrength(), monster.getMonsterType(), monster.getMonsterHealth());
             try {
                 Thread.sleep(1000);

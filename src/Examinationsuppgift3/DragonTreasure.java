dc89e12e60862c20865db2e254af98031587d8a9
Mattias <mattiash91@gmail.com>
2022-12-15 10:32:49 +0100

    Uppgift 3

--- src/Examinationsuppgift3/DragonTreasure.java
+++ src/Examinationsuppgift3/DragonTreasure.java
@@ -201,29 +201,29 @@ public class DragonTreasure {
         System.out.printf("| Available Directions: | %s | %s | %s | %s |%n%s%n%n%n", north, east, south, west, longs);
     }
 
-    public int fightSequence(Player player, Monster monster, int playerStrength, int playerHealth, String playerName, int monsterStrength, int monsterHealth, String monsterType){
-        while (monsterHealth > 0){
-            System.out.printf("%n%s attacks and deals %s damage!",monsterType, monsterStrength);
+    public int fightSequence(Player player, Monster monster, String playerName){
+        while (monster.getMonsterHealth() > 0){
+            System.out.printf("%n%s attacks and deals %s damage!",monster.getMonsterType(), monster.getMonsterStrength());
             player.setPlayerHealth(monster.Attack(monster.getMonsterStrength(), player.getPlayerHealth()));
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-            if (playerHealth < 0){
+            if (player.getPlayerHealth() < 0){
                 endGame();
             }
-            System.out.printf("%n%s attacks and deals %s damage!", playerName, playerStrength);
+            System.out.printf("%n%s attacks and deals %s damage!", playerName, player.getplayerStrength());
             monster.setMonsterHealth(player.Attack(player.getplayerStrength(), monster.getMonsterHealth()));
-            System.out.printf("%n%s has %s healthpoints left.", monsterType, monsterHealth);
+            System.out.printf("%n%s has %s healthpoints left.", monster.getMonsterType(), monster.getMonsterHealth());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
-        System.out.printf("%nThe %s has been defeated", monsterType);
-        return playerHealth;
+        System.out.printf("%nThe %s has been defeated", monster.getMonsterType());
+        return player.getPlayerHealth();
     }
     public static void endGame(){
         System.out.println("You died");
